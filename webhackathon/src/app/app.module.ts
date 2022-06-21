c1c3345a3d019b261ef5aea5388274f07195d1ec
Théo <theo.mouton@axopen.com>
2022-06-21 10:34:03 +0200

    feat(init): architecture

--- webhackathon/src/app/app.module.ts
+++ webhackathon/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { TerminalComponent } from './component/terminal/terminal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TerminalComponent
   ],
   imports: [
     BrowserModule,
