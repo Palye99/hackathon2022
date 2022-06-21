4458d201f4a7838e5d513558eafd2551e1f34d0d
Théo <theo.mouton@axopen.com>
2022-06-21 10:48:59 +0200

    feat(init): routing

--- webhackathon/src/app/app-routing.module.ts
+++ webhackathon/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './component/login/login.component';
+import {TerminalComponent} from './component/terminal/terminal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'terminal',
+    component: TerminalComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
